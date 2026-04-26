e9791d6fbfb5a75697d5075f5f45695d5e5aac5a
Keshaini Yogeswaran <yogeswarankeshaini@gmail.com>
2026-04-26 23:15:45 +0530

    fix: final merged App.jsx with resources + auth

--- nexahub-client/src/App.jsx
+++ nexahub-client/src/App.jsx
@@ -1,18 +1,90 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+// Auth + dashboards
+import AdminDashboard from "./pages/AdminDashboard.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import ForgotPassword from "./pages/ForgotPassword.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import ManagerDashboard from "./pages/ManagerDashboard.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import TechnicianDashboard from "./pages/TechnicianDashboard.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { ROLES } from "./auth/roles.js";
+
+// Resources module (your part)
 import ResourcesPage from "./pages/ResourcesPage";
 import ResourceDetailPage from "./pages/ResourceDetailPage";
 import AdminResourcesPage from "./pages/AdminResourcesPage";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ResourcesPage />} />
-        <Route path="/resource/:id" element={<ResourceDetailPage />} />
-        <Route path="/admin/resources" element={<AdminResourcesPage />} />
+        {/* PUBLIC */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* USER DASHBOARD */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute minRole={ROLES.USER}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* TECHNICIAN */}
+        <Route
+          path="/technician/dashboard"
+          element={
+            <ProtectedRoute minRole={ROLES.TECHNICIAN}>
+              <TechnicianDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* MANAGER */}
+        <Route
+          path="/manager/dashboard"
+          element={
+            <ProtectedRoute minRole={ROLES.MANAGER}>
+              <ManagerDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* ADMIN */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute minRole={ROLES.ADMIN}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* RESOURCES (STUDENT SIDE) */}
+        <Route path="/resources" element={<ResourcesPage />} />
+        <Route path="/resources/:id" element={<ResourceDetailPage />} />
+
+        {/* ADMIN RESOURCES */}
+        <Route
+          path="/admin/resources"
+          element={
+            <ProtectedRoute minRole={ROLES.ADMIN}>
+              <AdminResourcesPage />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* FALLBACK */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
